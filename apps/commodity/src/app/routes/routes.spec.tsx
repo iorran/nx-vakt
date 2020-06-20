7aef29ed82e1054c621b02c083fd214f381691cf
Iorran <iorran.castro@vakt.io>
2020-06-20 03:43:06 +0100

    (test) add github actions

---
+++ apps/commodity/src/app/routes/routes.spec.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+
+// import Routes from './routes';
+
+describe(' Routes', () => {
+  it('should render successfully', () => {
+    // const { baseElement } = render(<Routes />);
+    // expect(baseElement).toBeTruthy();
+  });
+});
